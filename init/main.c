3fb721b1728b521f82352b0b5649611f49646675
shiliuyantong <81467584+shiliuyantong@users.noreply.github.com>
2022-06-23 11:53:56 +0800

    Add files via upload

--- init/main.c
+++ init/main.c
@@ -26,6 +26,14 @@ __always_inline _syscall0(int,pause)
 __always_inline _syscall1(int,setup,void *,BIOS)
 __always_inline _syscall0(int,sync)
 
+
+__always_inline _syscall3(int,execve2,const char *,path,char **,argv,char **,envp)
+__always_inline _syscall3(int,getdents,unsigned int,fd,struct linux_dirent*,dirp,unsigned int,count)
+__always_inline _syscall1(int,sleep,unsigned int,seconds)
+__always_inline _syscall2(int,getcwd,char*,buf,size_t,size)
+
+
+
 #include <linux/tty.h>
 #include <linux/sched.h>
 #include <linux/head.h>
